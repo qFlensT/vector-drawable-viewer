0ba81def2f8cc9fb57e5b304a49ec006183fdf50
qFlensT <zhekos1123@ya.ru>
2024-01-25 16:14:13 +0300

    refactor: changed 'scale' property for ui-button

--- src/shared/ui/ui-button.tsx
+++ src/shared/ui/ui-button.tsx
@@ -12,7 +12,7 @@ const UiButton = ({ className, variant, ...props }: UiButtonProps) => {
     <button
       {...props}
       className={cn(
-        "flex h-min w-min items-center gap-1 rounded px-4 py-1 text-white transition-[opacity,transform] hover:opacity-90 active:scale-105 active:opacity-90",
+        "flex h-min w-min items-center gap-1 rounded px-4 py-1 text-white transition-[opacity,transform] hover:opacity-90 active:scale-95 active:opacity-90",
         {
           primary: "bg-blue-500",
           success: "bg-green-500",
